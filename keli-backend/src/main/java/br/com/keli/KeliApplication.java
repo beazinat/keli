7cec2e1c69e4b54fdb2060635600cc56c1e8b791
beazinat <beatrizgarodrigues@gmail.com>
2024-11-12 08:44:39 -0300

    :tada: First Commit!

---
+++ keli-backend/src/main/java/br/com/keli/KeliApplication.java
@@ -0,0 +1,13 @@
+package br.com.keli.keli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeliApplication.class, args);
+	}
+
+}
---
+++ keli-backend/src/test/java/br/com/keli/keli/KeliApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.keli.keli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
